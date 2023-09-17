bc4d01f63dc66e328b708a61489e01b87e2ade7f
RDLigeralde <144853011+RDLigeralde@users.noreply.github.com>
2023-09-17 14:12:36 -0400

    Add files via upload

--- sample_points.py
+++ sample_points.py
@@ -10,29 +10,80 @@ Original file is located at
 import jax.numpy as jnp
 import numpy as np
 
-def sample_moon(N, M, LM, ystr):
+"""
+Given joint dist. samples, sample product of marginal dists.
+"""
+def sample_marginals(N, M, data)
+
+  data_product = np.transpose([np.tile(data[:,0], N), np.repeat(data[:,1], N)])
+
+  data_product_get = data_product[np.random.choice(N ** 2, M, replace=False),:]
+
+  dp = jnp.array(sorted(data_product_get, key = lambda x: x[0]))
+
+  return dp
+
+"""
+Some synthetic examples for testing
+"""
+noise = .5
+
+#Moon example from previous paper
+def sample_moon(N, M):
 
   data = np.zeros([N,2])
   for i in range(N):
       data[i,0] = np.random.randn()
-      data[i,1]= 0.5 * np.random.randn() + data[i,0] ** 2 - 1
-
-  data_product = np.transpose([np.tile(data[:,0], N), np.repeat(data[:,1], N)])
-
-  data_product_get = data_product[np.random.choice(N**2, M, replace=False),:]
-
-  Zcurrent = jnp.array(sorted(data_product_get, key = lambda x: x[0]))
-  Zinitial = Zcurrent.copy()
+      data[i,1]= noise * np.random.randn() + data[i,0] ** 2 - 1
 
   Zjoint = jnp.array(sorted(data, key = lambda x: x[0]))
 
-  if LM:
-    #to estimate rho(x|ystr_lm)
-    Zlm = np.zeros((M,2))
-    Zlm[:,0] = np.random.randn(M)
-    Zlm[:,1] = np.ones(M) * ystr
-    Zlm = jnp.array(sorted(Zlm, key = lambda x: x[0]))
-    return [Zinitial, Zcurrent, Zjoint, Zlm]
+  Zinitial = sample_marginals(N, M, data)
+  Zcurrent = Zinitial.copy()
 
-  else:
-    return [Zinitial, Zcurrent, Zjoint]
+  return [Zinitial, Zcurrent, Zjoint]
+
+#Independent case: optimal map is the identity
+def sample_indep_gauss(N, M):
+
+  data = np.zeros([N,2])
+  for i in range(N):
+      data[i,0] = np.random.randn()
+      data[i,1]= np.random.randn()
+
+  Zjoint = jnp.array(sorted(data, key = lambda x: x[0]))
+
+  Zinitial = sample_marginals(N, M, data)
+  Zcurrent = Zinitial.copy()
+
+  return [Zinitial, Zcurrent, Zjoint]
+
+lam = 1
+
+def sample_expo(N, M):
+  data = np.zeros([N, 2])
+  for i in range(N):
+    data[i,0] = np.random.randn()
+    data[i,1] = noise * np.random.randn() + lam * jnp.exp(-lam * data[i,0])
+
+  Zjoint = jnp.array(sorted(data, key = lambda x: x[0]))
+
+  Zinitial = sample_marginals(N, M, data)
+  Zcurrent = Zinitial.copy()
+
+  return [Zinitial, Zcurrent, Zjoint]
+
+"""
+Source: name of csv with columns "X Samples", "Y Samples"
+"""
+
+def read_samples(source):
+
+  data = np.genfromtxt(source, delimiter = ',')
+
+  Zjoint = jnp.array(sorted(data, key = lambda x: x[0]))
+
+  Zinitial = sample_marginals(N, M, data)
+  Zcurrent = Zinitial.copy()
+
+  return [Zinitial, Zcurrent, Zjoint]
